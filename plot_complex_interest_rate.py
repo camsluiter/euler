6074b2e5f5125b0020bc8121bf6afb61ba223d26
camsluiter <33559324+camsluiter@users.noreply.github.com>
2023-06-24 10:56:02 -0700

    Update plot_complex_interest_rate.py
    recent

--- plot_complex_interest_rate.py
+++ plot_complex_interest_rate.py
@@ -1,28 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # interest rate
 
 prin=1
-t=2
-n=100
+t=np.pi
+n=2
 term = np.zeros(n,dtype=complex)
-rate = complex(0,np.pi)
+rate = complex(0,1)
 
-def term_calculator(rate, n):
+def term_calculator(rate, n, total_time):
     for i in range(n):
         if i ==0:
-            term[i]=(1+rate/n)
+            term[i]=(1+(rate*total_time)/n)
         else:
-            term[i]=term[i-1]*(1+rate/n) 
+            term[i]=term[i-1]*(1+(rate*total_time)/n) 
     return term
 
 def plot_terms(prin, total_time, n, rate, plot_type):
 
     x=[]
-    term = term_calculator(rate,n)
+    term = term_calculator(rate,n, total_time)
 
-    if(plot_type == "triangle"):
+    if(plot_type == "triangles"):
     #trying to demonstrate how the successive multiplications create a circle
         colors = plt.cm.get_cmap('hsv', 100)
         for i in range(n):
@@ -38,16 +39,21 @@ def plot_terms(prin, total_time, n, rate, plot_type):
         plt.ylim(-5,5)
         plt.show()
 
-    if(plot_type == "arrow"):
+    if(plot_type == "arrows"):
         plt.figure(1)
         term = np.insert(term,0,complex(1,0))
         plt.quiver(term[:-1].real, term[:-1].imag,term[1:].real-term[:-1].real, term[1:].imag-term[:-1].imag,scale=1, scale_units='xy',angles='xy')
         plt.grid()
         plt.show()
 
-    a = prin*(1+rate/n)**(n*t)
+    if(plot_type=="dots"):
+        #plt.figure(1)
+        sns.scatterplot(x=term[:].real, y=term[:].imag)
+        #plt.grid()
+        #plt.show()
+
     print(term)
-    return a
 
 
-plot_terms(prin, t, n,complex(0,np.pi),"arrow")
+
+plot_terms(prin, t, n,rate,"dots")
